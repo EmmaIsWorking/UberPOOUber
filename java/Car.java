e24f8bbaefa0a841107248bd90f3dd59f76832b6
Emmanuel García <dimple.pleod@gmail.com>
2020-04-03 12:53:17 -0600

    "metodo constructor en Java"

--- java/Car.java
+++ java/Car.java
@@ -1,6 +1,15 @@
 class Car {
     Integer id;
     String license;
-    String driver;
+    Account driver;
     Integer passenger;
+    
+    public Car(String license, Account driver){
+        this.license = license;
+        this.driver = driver;
+    }
+    void printDataCard(){
+        System.out.println("Licence: " + license + "," + "Driver: " + driver.name);
+    }
+    
 }
--- java/Main.java
+++ java/Main.java
@@ -1,16 +1,12 @@
 class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        car.license = "EMLI28";
-        car.driver = "Emmanuel";
+        Car car = new Car("EMLI28", new Account("Emmanuel García", "12JKH234GK"));
         car.passenger = 4;
-        System.out.println("Licencia de manejo: " + car.license);
+        System.out.println("Nombre: " + car.driver.name +  "Licencia de manejo: " + car.license);
 
-        Car car2 = new Car();
-        car2.license = "JDSF8";
-        car2.driver = "Llanas";
+        Car car2 = new Car("HJK34", new Account("Edoardo Llanas", "LKHJA451SD90"));
         car2.passenger = 4;
-        System.out.println("Licencia de manejo" + car2.license);
+        System.out.println("Nombre: " + car.driver.name + "Licencia de manejo" + car2.license);
 
     }
 }
