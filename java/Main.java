6cfcc0663b643e13698820594d109e1284488cf2
Emmanuel García <dimple.pleod@gmail.com>
2020-04-03 12:03:16 -0600

    creando instancias

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,16 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        Car car = new Car();
+        car.license = "EMLI28";
+        car.driver = "Emmanuel";
+        car.passenger = 4;
+        System.out.println("Licencia de manejo: " + car.license);
+
+        Car car2 = new Car();
+        car2.license = "JDSF8";
+        car2.driver = "Llanas";
+        car2.passenger = 4;
+        System.out.println("Licencia de manejo" + car2.license);
+
     }
 }
