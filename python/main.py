c10eb508b3211678c5fd8cbb6775134e5d506a11
Emmanuel García <dimple.pleod@gmail.com>
2020-04-03 12:15:22 -0600

    Creando Objetos en Python

--- python/main.py
+++ python/main.py
@@ -1,2 +1,13 @@
-if if __name__ == "__main__":
-    print('Hola mundo')
+from car import Car
+
+if __name__ == "__main__":
+    print("Objetos en Python")
+    car = Car()
+    car.license = "IOU87"
+    car.driver = "Emmanuel García"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "87SJKH"
+    car2.driver = "Llanas Meza"
+    print(vars(car2))
