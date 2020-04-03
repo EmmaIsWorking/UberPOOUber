ca536501e760a8f1ffd40cbdf4cf51eb639b589d
Emmanuel García <dimple.pleod@gmail.com>
2020-04-03 16:41:18 -0600

    metodo constructor en Python

--- python/main.py
+++ python/main.py
@@ -1,13 +1,10 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
     print("Objetos en Python")
-    car = Car()
-    car.license = "IOU87"
-    car.driver = "Emmanuel García"
+    car = Car('ADSJ12', Account('Emmanuel Garcia', 'H29S788987KNB'))
     print(vars(car))
 
-    car2 = Car()
-    car2.license = "87SJKH"
-    car2.driver = "Llanas Meza"
+    car2 = Car('LJ82LKJ', Account('Edoardo Llanas', 'ASDLKJF908AD '))
     print(vars(car2))
