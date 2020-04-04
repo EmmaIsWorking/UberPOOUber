45b7f590061c17e9065d2dd23888836e4e5b49a4
Emmanuel García <dimple.pleod@gmail.com>
2020-04-04 15:01:22 -0600

    Setters and Getters

--- java/Car.java
+++ java/Car.java
@@ -2,14 +2,30 @@ class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passenger;
-    
-    public Car(String license, Account driver){
+    private Integer passegenger;
+
+    public Car(String license, Account driver) {
         this.license = license;
         this.driver = driver;
     }
-    void printDataCard(){
-        System.out.println("Licence: " + license + "," + "Driver: " + driver.name);
+
+    void printDataCar() {
+        if (passegenger != null) {
+            System.out.println("License: " + license + " Name Driver: " + driver.name + " Passengers: " + passegenger);
+        }
+
     }
-    
+
+    public Integer getPassenger() {
+        return passegenger;
+    }
+
+    public void setPassenger(Integer passenger) {
+        if (passenger == 4) {
+            this.passegenger = passenger;
+        } else {
+            System.out.println("Necesitas asignar 4 pasajeros");
+        }
+    }
+
 }
--- java/Main.java
+++ java/Main.java
@@ -1,12 +1,11 @@
 class Main {
     public static void main(String[] args) {
-        Car car = new Car("EMLI28", new Account("Emmanuel García", "12JKH234GK"));
-        car.passenger = 4;
-        System.out.println("Nombre: " + car.driver.name +  "Licencia de manejo: " + car.license);
-
-        Car car2 = new Car("HJK34", new Account("Edoardo Llanas", "LKHJA451SD90"));
-        car2.passenger = 4;
-        System.out.println("Nombre: " + car.driver.name + "Licencia de manejo" + car2.license);
+        System.out.println("Hola Mundo");
+        UberX uberX = new UberX("JHA8892", new Account("Emmanuel García", "183098JLKAD"), "VolksWagen", "Vocho");
+        uberX.setPassenger(4);
+        uberX.printDataCar();
+        // Car car2 = new Car("HJK34", new Account("Edoardo Llanas", "LKHJA451SD90"));
+        // System.out.println("Nombre: " + car.driver.name + "Licencia de manejo" + car2.license);
 
     }
 }
