bd49197fad4c02415f2352ffb23742a369fadd4f
Emmanuel García <dimple.pleod@gmail.com>
2020-04-03 19:10:12 -0600

    herencia en Java

---
+++ java/UberBlack.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class UberBlack extends Car{
+    Map<String, ArrayList<String, Integer>> typeCarAcepted;
+    ArrayList<String> seatsMaterial;
+
+
+    public UberBlack(String license, Account driver, 
+    Map<String, ArrayList<String, Integer>> typeCarAcepted,
+    ArrayList<String> seatsMaterial){
+        super(license, driver);
+        this.seatsMaterial;
+        this.typeCarAcepted;
+    }
+    
+}
---
+++ java/UberVan.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class UberVan extends Car {
+    Map<String, ArrayList<String, Integer>> typeCarAcepted;
+    ArrayList<String> seatsMaterial;
+
+    public UberVan(String license, Account driver, 
+    Map<String, ArrayList<String, Integer>> typeCarAcepted,
+    ArrayList<String> seatsMaterial){
+        super(license, driver);
+        this.seatsMaterial;
+        this.typeCarAcepted;
+    }
+
+}
